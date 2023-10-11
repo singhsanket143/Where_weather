26380bae1831da5e0d8ef5671cb7bfdfd2bb17f5
Sanket Singh <singhsanket143@gmail.com>
2023-10-11 22:37:30 +0530

    Added basic layout with sidebar and main data partition

--- src/Components/MainData/MainData.tsx
+++ src/Components/MainData/MainData.tsx
@@ -1,8 +1,8 @@
 function MainData() {
     return (
-        <>
+        <div className="basis-9/12 text-black h-full bg-[#f6f6f8] rounded-br-3xl rounded-tr-3xl">
             MainData
-        </>
+        </div>
     )
 }
 
--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,8 +1,8 @@
 function Sidebar() {
     return (
-        <>
+        <div className="basis-3/12 flex flex-col items-center text-black h-full rounded-tl-3xl rounded-bl-3xl" style={{background: 'rgba(255,255,255,0.81'}}>
             Sidebar
-        </>
+        </div>
     )
 }
 
