8ef799994fa69338be0c36c02ccf1be374e03a58
Sanket Singh <singhsanket143@gmail.com>
2023-10-13 22:05:09 +0530

    Added divder and lower half

--- src/Components/Sidebar/Lowerhalf.tsx
+++ src/Components/Sidebar/Lowerhalf.tsx
@@ -1,7 +1,37 @@
+import { BsDropletFill } from "react-icons/bs";
+import { AiFillCloud } from "react-icons/ai";
+import { MdLocationPin } from "react-icons/md";
+
 function Lowerhalf() {
     return (
-        <div className="h-[50%] w-full p-4"> 
-            Lower half
+        <div className="h-[50%] w-full p-4 flex flex-col justify-between"> 
+            <div className="flex flex-col">
+                {/* precipitation */}
+                <div className="flex items-center gap-4 my-2 text-sm">
+                    <div>
+                        <AiFillCloud />
+                    </div>
+                    <div>
+                        Partially cloudy
+                    </div>
+                </div>
+
+                <div className="flex items-center gap-4 my-2 text-sm">
+                    <div>
+                        <BsDropletFill />
+                    </div>
+                    <div>
+                        Perc - 10%
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex gap-2 items-center">
+                {/* location */}
+                <div><MdLocationPin /></div>
+                
+                <div>Bengaluru, KA, India</div>
+            </div>
         </div>
     )
 
--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -5,6 +5,11 @@ function Sidebar() {
     return (
         <div className="basis-3/12 flex flex-col items-center justify-center text-black h-full rounded-tl-3xl rounded-bl-3xl" style={{background: 'rgba(255,255,255,0.81'}}>
             <Upperhalf />
+            
+            <div className="h-[1px] w-[87%] bg-[#e9ecef] mt-8 rounded-md">
+
+            </div>
+
             <Lowerhalf />
         </div>
     )
